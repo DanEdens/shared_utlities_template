919f85f1f75e38e973beff5e8f55b51268bcbdbb
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-09-25 14:43:18 -0400

    Edits run config and ignores .pyc

--- .bin/start-server.cmd
+++ .bin/start-server.cmd
@@ -1,1 +1,3 @@
-mosquitto_sub -h %awsip% -p %awsport% -v -t "TestKit/#" | xargs -o -r -I {} python __main__.py {}
+REM call "%cd%/update.cmd"
+
+mosquitto_sub -h %awsip% -p %awsport% -v -t "TestKit/#" | xargs -o -r -I {} python __main__.py --server {}
--- .bin/update.cmd
+++ .bin/update.cmd
@@ -1,6 +1,4 @@
 cd ..
-type groups.py > Archive/groups.bak.py
-type exclude.dat > Archive/exclude.bak.dat
 git fetch
 git switch --discard-changes production
 git pull

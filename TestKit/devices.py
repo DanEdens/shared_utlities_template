b77d8d03eb81c1f82c61034dbcf496438f116884
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-09-25 15:13:06 -0400

    continues work on arguments and docstrings
    Adds a device list

--- TestKit/devices.py
+++ TestKit/devices.py
@@ -1,1 +1,12 @@
 # mosquitto_pub -h %awsip% -p %awsport% -t "loggernet/devicelist" -f "devicelist.txt"
+deviceList = [
+        "S10",
+        "bs"
+        ]
+
+testList = [
+        "pullDeviceList",
+        # "pubFullStatus",
+        "start-server",
+        "update"
+        ]
--- __main__.py
+++ __main__.py
@@ -12,10 +12,12 @@ from TestKit.config import fetch_config
 
 logger = makeLogger('testkit')
 
+# Variable WatchList to be logged by the Server
 varlist = [
         'DeNa'
         ]
 
+# List of commands available to the Server
 commands = [
         'pullDeviceList',
         'start-server'
@@ -24,8 +26,7 @@ commands = [
 
 def script(filename):
     """
-    Runs a script on the local device
-    This is a security precaution to limit available commands
+    Runs .bin scripts as subprocesses
     """
     _path = os.environ['bin'] + f'\\{filename}.cmd'
     if filename in commands:
@@ -33,24 +34,40 @@ def script(filename):
 
 
 def parseMessage(msg):
+    """
+    Handles message payload and logs events
+    Scripts in .bin must be hardcoded into the command list above
+    :param msg:
+    """
     key = msg.split('/')
     value = key[2].split(' ')
     if value[0] in commands:
         try:
+            # Log script invocations
+            logger.info(f"Running Command {value[0]}: {value[1]}")
             script(value[0])
         except:
             pass
-        print(f"Running Command {value[0]}: {value[1]}")
     if value[0] in varlist:
-        # Since all values are checked when called,
-        # Value changes only need to be logged, not saved
-        print(f"{value[0]} set to: {value[1]}")
+        # Log variable changes
+        logger.info(f"{value[0]} set to: {value[1]}")
 
 
 if __name__ == "__main__":
+    """
+        Main Thread 
+    """
+    logger.debug(thread.args)
+
     if thread.args.server:
         print(sys.argv)
         # parseMessage(sys.argv[2])
-    elif thread.args.post-docs:
+    elif thread.args.make_docs:
         print('Failed')
-        # DeNa = fetch_config()
+    elif thread.args.post_docs:
+        print('Failed')
+    elif thread.args.device:
+
+        DeNa = fetch_config()
+    elif thread.args.test:
+        print('Failed')
